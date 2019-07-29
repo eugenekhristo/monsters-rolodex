92d06564de67334cf4d604f66db6dba5b0a941b2
Eugene Khristo <marteau9292@gmail.com>
2019-07-29 19:10:57 +0300

    Updates

---
+++ precache-manifest.ccb9dab21787ecc20896ea956af3b985.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3bd1fa7da659bc1646ecc2b5b9f9c7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "28838348bee89aeaa4b7",
+    "url": "/monsters-rolodex/static/css/main.e8f73318.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "28838348bee89aeaa4b7",
+    "url": "/monsters-rolodex/static/js/main.38318bb4.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
